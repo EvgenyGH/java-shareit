94ca40c216d6d25adcba8b71833f6bca51da4524
Evgeny S <j3000@bk.ru>
2022-08-20 11:58:16 +0300

    feat: Add integration tests ItemRequestServiceImpl and dataBase, ItemRequestRepository tests, ItemRequestController tests, ItemRequestDto and ItemRequestDtoWithResponse tests.

--- src/test/java/ru/practicum/shareit/request/IntegrationRequestServiceAndDbTest.java
+++ src/test/java/ru/practicum/shareit/request/IntegrationRequestServiceAndDbTest.java
@@ -21,6 +21,7 @@ import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,7 +37,6 @@ public class IntegrationRequestServiceAndDbTest {
 
     private User user;
     private ItemRequest itemRequest;
-    private Item item;
 
     @BeforeEach
     void initialize() {
@@ -45,8 +45,8 @@ public class IntegrationRequestServiceAndDbTest {
         ItemRequestDto itemRequestDto = itemRequestService.addRequest(user.getId(), ItemRequestDtoMapper
                 .itemRequestToDto(itemRequest));
         itemRequest.setId(itemRequestDto.getId());
-        item = new Item(15L, "item name 15", "description 15", true, user, itemRequest);
-        item = itemService.addItem(ItemDtoMapper.ItemToDto(item), user.getId());
+        Item item = new Item(15L, "item name 15", "description 15", true, user, itemRequest);
+        itemService.addItem(ItemDtoMapper.ItemToDto(item), user.getId());
     }
 
     @Test
@@ -77,10 +77,10 @@ public class IntegrationRequestServiceAndDbTest {
         List<Item> items = queryItem.setParameter("id", itemRequest.getId()).getResultList();
 
 
-        List<ItemRequestDtoWithResponse> requestDtoDb = requestsDb.stream().map(ItemRequest ->
+        List<ItemRequestDtoWithResponse> requestDtoDb = requestsDb.stream().map(itemRequest ->
                         ItemRequestDtoMapper.itemRequestToDtoWithResponse(itemRequest
                                 , items.stream().map(ItemDtoMapper::ItemToDto).collect(Collectors.toList())))
-                        .collect(Collectors.toList());
+                .collect(Collectors.toList());
 
         assertThat(requestDtoDb.size()).isEqualTo(requests.size());
         assertThat(requestDtoDb.get(0).getId()).isEqualTo(requests.get(0).getId());
@@ -88,18 +88,83 @@ public class IntegrationRequestServiceAndDbTest {
         assertThat(requestDtoDb.get(0).getItems()).isEqualTo(requests.get(0).getItems());
     }
 
+    @Test
+    @Transactional
+    void getAllRequestsTest() {
+        User userTest = userService.addUser(new User(15L, "user name new", "email@new.com"));
+        ItemRequest itemRequestTest = new ItemRequest(20L, "description new", userTest
+                , LocalDateTime.now());
+        ItemRequestDto itemRequestDtoTest = itemRequestService.addRequest(userTest.getId(), ItemRequestDtoMapper
+                .itemRequestToDto(itemRequestTest));
+        itemRequestTest.setId(itemRequestDtoTest.getId());
+        Item itemTest = new Item(15L, "item name new", "description new", true
+                , userTest, itemRequestTest);
+        itemService.addItem(ItemDtoMapper.ItemToDto(itemTest), userTest.getId());
 
-    /*//Получить список запросов, созданных другими пользователями.
-    //Запросы сортируются от более новых к более старым.
-    //Результаты возвращаются постранично.
-    //from — индекс первого элемента, size — количество элементов для отображения.
-    List<ItemRequestDtoWithResponse> getAllRequests(Optional<Integer> fromOpt, Optional<Integer> sizeOpt
-            , Long ownerId);
+        List<ItemRequestDtoWithResponse> allRequests = itemRequestService.getAllRequests(
+                Optional.of(0), Optional.of(5), user.getId());
 
-    //Получить данные об одном конкретном запросе вместе с данными об ответах по id.
-    ItemRequestDtoWithResponse getRequestById(Long userId, Long requestId);
+        TypedQuery<ItemRequest> queryRequest = entityManager.createQuery(
+                "SELECT i FROM ItemRequest i " +
+                        "WHERE i.requestor.id <> :id", ItemRequest.class);
 
-    //Получить запрос по id.
-    ItemRequest getItemRequestById(Long requestId);*/
+        List<ItemRequest> requestsDb = queryRequest.setParameter("id", user.getId()).getResultList();
+
+        TypedQuery<Item> queryItem = entityManager.createQuery(
+                "SELECT i FROM Item i " +
+                        "WHERE i.request.id = :id", Item.class);
+
+        List<Item> items = queryItem.setParameter("id", itemRequestTest.getId()).getResultList();
+
+
+        List<ItemRequestDtoWithResponse> requestDtoDb = requestsDb.stream().map(itemRequest ->
+                        ItemRequestDtoMapper.itemRequestToDtoWithResponse(itemRequest
+                                , items.stream().map(ItemDtoMapper::ItemToDto).collect(Collectors.toList())))
+                .collect(Collectors.toList());
+
+        assertThat(requestDtoDb.size()).isEqualTo(allRequests.size());
+        assertThat(requestDtoDb.get(0).getId()).isEqualTo(allRequests.get(0).getId());
+        assertThat(requestDtoDb.get(0).getDescription()).isEqualTo(allRequests.get(0).getDescription());
+        assertThat(requestDtoDb.get(0).getItems()).isEqualTo(allRequests.get(0).getItems());
+    }
+
+    @Test
+    @Transactional
+    void getRequestByIdTest() {
+        ItemRequestDtoWithResponse request = itemRequestService.getRequestById (user.getId(), itemRequest.getId());
+
+        TypedQuery<ItemRequest> queryRequest = entityManager.createQuery(
+                "SELECT i FROM ItemRequest i " +
+                        "WHERE i.requestor.id = :id", ItemRequest.class);
+
+        ItemRequest requestsDb = queryRequest.setParameter("id", user.getId()).getSingleResult();
+
+        TypedQuery<Item> queryItem = entityManager.createQuery(
+                "SELECT i FROM Item i " +
+                        "WHERE i.request.id = :id", Item.class);
+
+        List<Item> items = queryItem.setParameter("id", itemRequest.getId()).getResultList();
+
+        ItemRequestDtoWithResponse requestDtoDb = ItemRequestDtoMapper.itemRequestToDtoWithResponse(requestsDb
+                                , items.stream().map(ItemDtoMapper::ItemToDto).collect(Collectors.toList()));
+
+        assertThat(requestDtoDb.getId()).isEqualTo(request.getId());
+        assertThat(requestDtoDb.getDescription()).isEqualTo(request.getDescription());
+        assertThat(requestDtoDb.getItems()).isEqualTo(request.getItems());
+    }
+
+    @Test
+    @Transactional
+    void getItemRequestByIdTest() {
+        ItemRequest request = itemRequestService.getItemRequestById (itemRequest.getId());
+
+        TypedQuery<ItemRequest> queryRequest = entityManager.createQuery(
+                "SELECT i FROM ItemRequest i " +
+                        "WHERE i.requestor.id = :id", ItemRequest.class);
+
+        ItemRequest requestsDb = queryRequest.setParameter("id", user.getId()).getSingleResult();
+
+        assertThat(request).isEqualTo(requestsDb);
+    }
 }
 
