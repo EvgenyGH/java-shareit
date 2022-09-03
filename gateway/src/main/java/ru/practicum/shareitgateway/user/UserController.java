d0a06f277f6428e3e31a220ca3ee4b9af324d99d
Evgeny S <j3000@bk.ru>
2022-09-03 16:21:14 +0300

    feat: Add UserController and UserClient.

--- gateway/src/main/java/ru/practicum/shareitgateway/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareitgateway/user/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareitgateway.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,36 +13,42 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Slf4j
 public class UserController {
     private final UserClient client;
 
     //Добавить пользователя
     @PostMapping
     public ResponseEntity<Object> addUser(@Valid @RequestBody User user) {
-        return client.addUser(user);//userService.addUser(user);
+        log.trace("Gateway: addUser: {}", user);
+        return client.addUser(user);
     }
 
     //Обновить данные пользователя
     @PatchMapping("/{userId}")
     public ResponseEntity<Object> updateUser(@PathVariable long userId, @RequestBody User user) {
-        return client.updateUser(userId, user); //userService.updateUser(user);
+        log.trace("Gateway: updateUser: userId={} user={}", userId, user);
+        return client.updateUser(userId, user);
     }
 
     //Получить пользователя по id
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getUserById(@PathVariable long userId) {
-        return client.getUserById(userId); //userService.getUserById(userId);
+        log.trace("Gateway: getUserById: userId={}", userId);
+        return client.getUserById(userId);
     }
 
     //Удалить пользователя по id
     @DeleteMapping("/{userId}")
     public ResponseEntity<Object> deleteUserById(@PathVariable long userId) {
-        return client.deleteUserById(userId); //userService.deleteUserById(userId);
+        log.trace("Gateway: deleteUserById: userId={}", userId);
+        return client.deleteUserById(userId);
     }
 
     //Получить всех пользователей
     @GetMapping
     public ResponseEntity<Object> getAllUsers() {
-        return client.getAllUsers(); //userService.getAllUsers();
+        log.trace("Gateway: getAllUsers");
+        return client.getAllUsers();
     }
 }
--- gateway/src/main/java/ru/practicum/shareitgateway/user/client/UserClientImpl.java
+++ gateway/src/main/java/ru/practicum/shareitgateway/user/client/UserClientImpl.java
@@ -3,17 +3,45 @@ package ru.practicum.shareitgateway.user.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareitgateway.client.BasicClient;
+import ru.practicum.shareitserver.user.User;
 
 @Component
-public class UserClientImpl extends BasicClient implements UserClient{
+public class UserClientImpl extends BasicClient implements UserClient {
     @Autowired
-    public UserClientImpl(@Value("${SERVER_URL}") String url,  RestTemplateBuilder builder) {
-        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(url + "/items")).
-                requestFactory(HttpComponentsClientHttpRequestFactory::new).
+    public UserClientImpl(@Value("${SERVER_URL}") String url, RestTemplateBuilder builder) {
+        super(builder.
+                requestFactory(HttpComponentsClientHttpRequestFactory.class).
+                uriTemplateHandler(new DefaultUriBuilderFactory(url + "/users")).
                 build());
+    }
+
+    @Override
+    public ResponseEntity<Object> addUser(User user) {
+        return post(user);
+    }
+
+    @Override
+    public ResponseEntity<Object> updateUser(long userId, User user) {
+        return patch(userId, "/" + userId, user, null);
+    }
+
+    @Override
+    public ResponseEntity<Object> getUserById(long userId) {
+        return get(userId, "/" + userId, null);
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteUserById(long userId) {
+        return delete(userId, "/" + userId, null);
+    }
+
+    @Override
+    public ResponseEntity<Object> getAllUsers() {
+        return get(null);
     }
 }
